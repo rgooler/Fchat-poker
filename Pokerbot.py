277c05606d68cb1227de30ff97d3316ebb7f9d3c
Ryan Gooler <cheetahmorph@gmail.com>
2012-04-03 23:38:49 -0700

    Basic dealing support

--- Pokerbot.py
+++ Pokerbot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from Fchat import Fchat
+from Deck import Deck
 
 class Pokerbot(object):
     """
@@ -10,7 +11,8 @@ class Pokerbot(object):
 
     #Set this to 0 to not reconnect, or the seconds to wait until reconnecting 
     reconnect = 60
-    
+    deck = Deck()
+
     def __init__(self):
         self.FC = Fchat()
 
@@ -23,6 +25,7 @@ class Pokerbot(object):
         #I have not found it.
         self.FC.handle_FRL = self.handle_FRL
         self.FC.handle_PRI = self.handle_PRI
+        self.FC.handle_CIU = self.handle_CIU
 
     ###########################################################################
     ### handlers for bot behavior                                           ###
@@ -36,16 +39,26 @@ class Pokerbot(object):
             self.adminCommand(data['character'],data['message'])
         else:
             self.normalCommand(data['character'],data['message'])
-        
+    
+    def handle_CIU(self,data):
+        #I was invited to a room
+        if data['sender'] in self.adminUsers:
+            self.FC.send('JCH',{'channel': data['channel']} )
+
     ###########################################################################
     ### Most bot logic goes here                                            ###
     ###########################################################################
     def adminCommand(self,character,command):
-        r  ="\n"
-        r +="Admin Help file\n"
-        r +=self.cname + "\n"
-        
-        self.FC.send_PRI(character,r)
+        if command == "!deal":
+            c = self.deck.deal()
+            hand = character + u': ' + ' '.join(c)
+            self.FC.send_PRI(character,hand)
+        else:
+            r  ="\n"
+            r +="Admin Help file\n"
+            r +=self.cname + "\n"
+            r +="!deal - Deals a hand\n"
+            self.FC.send_PRI(character,r)
 
     def normalCommand(self,character,command):
         message = "Sorry, you are not an admin, I cannot help you."
