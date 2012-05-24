62db1c7dd9f242e6af16e417e0b8cd2eecbd920e
Ryan Gooler <cheetahmorph@gmail.com>
2012-05-23 20:38:26 -0700

    Make black suits black

--- Deck.py
+++ Deck.py
@@ -44,6 +44,7 @@ class Deck(object):
             h = self.deal()
         hand = u''+h[0]+u' '+h[1]+u' '+h[2]+u' '+h[3]+u' '+h[4]
         hand = hand.replace(u'♥',u'[color=red]♥[/color]').replace(u'♦',u'[color=red]♦[/color]')
+        hand = hand.replace(u'♠',u'[color=black]♠[/color]').replace(u'♣',u'[color=black]♣[/color]')
         return hand
 
 if __name__ == "__main__":
